f6a0cf8149f53ff545d11628c7f02ab1db394d56
Noman Hasan <nomanbinhussein@gmail.com>
2018-04-26 01:08:19 +0600

    Func try

--- 2.Basic/Program.cs
+++ 2.Basic/Program.cs
@@ -7,6 +7,19 @@ namespace _2.Basic
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            char c = 'A';
+
+            Console.WriteLine(c);
+            Console.WriteLine(isEven(23));
+            Console.WriteLine(divide(y:100, x:25 ));
+        }
+
+        static Func<int, bool> isEven = x => x % 2 == 0; 
+
+        static float divide (int x, int y ) {
+            Console.WriteLine(x + " - and y is " + y);
+            return (float)x / (float)y;
         }
     }
 }
