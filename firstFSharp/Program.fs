4c8e196464b479c308445b3cf8afdf82f4bedfb8
Noman Hasan <nomanbinhussein@gmail.com>
2018-04-26 19:23:53 +0600

    first F#

---
+++ firstFSharp/Program.fs
@@ -0,0 +1,18 @@
+﻿open System // Get access to functionality in System namespace.
+
+// Function: takes a name and produces a greeting.
+let getGreeting name =
+    sprintf "Hello, %s! Isn't F# great?" name
+
+// Use the EntryPoint attribute to run the program.
+[<EntryPoint>]
+let main args =
+    // Define a list of names
+    let names = [| "Don"; "Julia"; "Xi" |]
+
+    // Print a fun greeting for each name!
+    names
+    |> Array.map getGreeting
+    |> Array.iter (fun greeting -> printfn "%s" greeting)
+
+    0
