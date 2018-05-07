655be80c161ec9690e5509806745584681480d61
Noman Hasan <nomanbinhussein@gmail.com>
2018-05-07 23:42:22 +0600

    Nutshell on 4

---
+++ nutshell/3.types/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3.types
+{
+
+
+  interface I1
+  {
+    void Foo();
+  }
+  interface I2
+  {
+    void Foo();
+  }
+
+  class Program
+  {
+
+
+    class ImplementTwo : I1, I2
+    {
+      void I1.Foo()
+      {
+        Console.WriteLine("Foo of Implement2");
+      }
+
+      public void Foo()
+      {
+        Console.WriteLine("Foo of I2. ");
+      }
+    }
+
+
+    public enum BorderSides
+    {
+      None = 1,
+      Left = 2,
+      Right = 3,
+      Top = 4,
+      Bottom = 5
+    }
+
+
+    public class Animal
+    {
+    }
+
+    public class Bear : Animal
+    {
+
+    }
+
+    public class Camel : Animal
+    {
+
+    }
+
+    public interface IPoppable<out T>
+    {
+      T Pop();
+    }
+
+    public class Stack<T>
+    {
+      int position;
+      T[] data = new T[100];
+      public void Push(T obj) => data[position++] = obj;
+      public T Pop()
+      => data[--position];
+    }
+
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+
+      var bears = new Stack<Bear>();
+      bears.Push(new Bear());
+      // Bears implements IPoppable<Bear>. We can convert to IPoppable<Animal>:
+      IPoppable<Animal> animals = bears as IPoppable<Animal>;
+      // Legal
+      Animal a = animals.Pop();
+
+    }
+
+  }
+}
---
+++ nutshell/4.advanced/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.advanced
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+
+
+        foreach(var i in EvenNumbers(Fibonacci(20))) {
+            Console.WriteLine(i);
+        }
+
+    }
+
+
+    public static IEnumerable<int> Fibonacci(int length)
+    {
+      for (int i = 0, prevFib = 1, curFib = 1; i < length; i++)
+      {
+        yield return prevFib;
+        int newFib = curFib + prevFib;
+        prevFib = curFib;
+        curFib = newFib;
+      }
+    }
+
+    public static IEnumerable<int> EvenNumbers (IEnumerable<int> numbers) {
+        foreach(int i in numbers) {
+            if (i % 2 == 0) {
+                yield return i;
+            }
+        }
+    }
+  }
+}
